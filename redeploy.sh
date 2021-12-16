4d12a208e6e876648d290adc2068f45112b7e9a0
Caleb Carlson <caleb.carlson@hpe.com>
2021-12-15 19:51:49 -0700

    Fix redeploy

--- redeploy.sh
+++ redeploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git fetch && git pull && docker build -t sustain/sparkplayground . && docker push sustain/sparkplayground && kubectl delete job.batch/sparkplayground && kubectl apply -f deploy/job.yaml
+git fetch && git pull && docker build -t sustain/sparkplayground . && docker push sustain/sparkplayground && kubectl apply -f deploy/job.yaml
