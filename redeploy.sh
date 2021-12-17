48d55e2ee3dbb60a3d40376c3be4a1b476a4cb72
Caleb Carlson <caleb.carlson@hpe.com>
2021-12-17 09:24:33 -0700

    Use default Partitioner

--- redeploy.sh
+++ redeploy.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-git fetch && git pull && docker build -t sustain/sparkplayground . && docker push sustain/sparkplayground && kubectl delete -f deploy/job.yaml; kubectl apply -f deploy/job.yaml
+git fetch \
+  && git pull \
+  && docker build -t sustain/sparkplayground . \
+  && docker push sustain/sparkplayground \
+  && kubectl delete -f deploy/job.yaml
+
+kubectl apply -f deploy/job.yaml
+
+if [ $? ]; then
+  while [ $(kubectl get pod/sparkplayground | grep "ContainerCreating") ]; do
+    sleep 1
+  done
+
+  kubectl get pod/sparkplayground | grep "Running" && kubectl logs -f pod/sparkplayground
+fi
